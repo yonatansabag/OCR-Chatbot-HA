4e8b3430f9640ea8f0edab7ad24db72c7f47b6e8
yonatansabag <81235287+yonatansabag@users.noreply.github.com>
2025-01-08 22:11:36 +0200

    Fix generate_kb.py to create the data directory if not exists

--- Part2/generate_kb.py
+++ Part2/generate_kb.py
@@ -1,8 +1,14 @@
+import os
 from app.utils import generate_embeddings_for_html_files_with_chunking
 
 if __name__ == "__main__":
     DATA_DIR = "./data"
     OUTPUT_FILE = "knowledge_base_embeddings_chunked.json"
 
+    # Create the data directory if it does not exist
+    if not os.path.exists(DATA_DIR):
+        os.makedirs(DATA_DIR)
+
     # Generate embeddings for the HTML files
     generate_embeddings_for_html_files_with_chunking(DATA_DIR, OUTPUT_FILE)
+    
